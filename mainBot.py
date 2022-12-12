9bc85ef9df538db15ee3528b1d2c87ccf12f911c
FabriCrespo <88858421+FabriCrespo@users.noreply.github.com>
2022-12-12 17:40:39 -0400

    Add files via upload

--- mainBot.py
+++ mainBot.py
@@ -1,95 +1,82 @@
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer
-import numpy 
+stemmer = LancasterStemmer()
+import numpy
 import tflearn
 import tensorflow
-import json 
+import json
 import random
 import pickle
-
+from tensorflow.python.framework import ops
 
 with open("contenido.json", encoding='utf-8') as archivo:
-	datos = json.load(archivo)
-try:
-	with open("variables.pickle", "rb") as archivoPickle:
-		palabras, tags, entrenamiento, salida = pickle.load(archivoPickle)
-except:
+ datos = json.load(archivo)
 
-	palabras=[]
-	tags=[]
-	auxX=[]
-	auxY=[]
+palabras = []
+tags = []
+auxX = []
+auxY = []
 
-	for contenido in datos["contenido"]:
-		for patrones in contenido["patrones"]:
-			auxPalabra = nltk.word_tokenize(patrones)
-			palabras.extend(auxPalabra)
-			auxX.append(auxPalabra)
-			auxY.append(contenido["tag"])
+for  contenido in datos["contenido"]:
+	for patrones in contenido["patrones"]:
+		auxPalabra = nltk.word_tokenize(patrones)
+		palabras.extend(auxPalabra)
+		auxX.append(auxPalabra)
+		auxY.append(contenido["tag"])
 
-			if contenido["tag"] not in tags:
-				tags.append(contenido["tag"])
+		if contenido["tag"] not in tags:
+			tags.append(contenido["tag"])
 
+palabras = [stemmer.stem(w.lower()) for w in palabras]
+palabras = sorted(list(set(palabras)))
+tags = sorted(tags)
 
-	palabras = [stemmer.stem(w.lower()) for w in palabras if w!="?"]
-	palabras = sorted(list(set(palabras)))
-	tags = sorted(tags)
+entrenamiento = []
+salida = []
 
-	entrenamiento = []
-	salida=[]
+salidaVacia = [0 for _ in range(len(tags))]
 
-	salidaVacia = [0 for _ in range(len(tags))]
+for x, documento in enumerate(auxX):
+	cubeta = []
+	auxPalabra = [stemmer.stem(w.lower()) for w in documento]
+	for w in palabras:
+		if w in auxPalabra:
+			cubeta.append(1)
+		else:
+			cubeta.append(0)
+	filaSalida = salidaVacia[:]
+	filaSalida[tags.index(auxY[x])]=1
+	entrenamiento.append(cubeta)
+	salida.append(filaSalida)
 
-	for x, documento in enumerate(auxX): 
-		cubeta = []
-		auxPalabra = [stemmer.stem(w.lower()) for w in documento]
-		for w in palabras:
-			if w in auxPalabra: 
-				cubeta.append(1)
-			else:
-				cubeta.append(0)
-		filaSalida = salidaVacia[:]
-		filaSalida[tags.index(auxY[x])]=1
-		entrenamiento.append(cubeta)
-		salida.append(filaSalida)
-
-	entrenamiento = numpy.array(entrenamiento)
-	salida = numpy.array(salida) 
-	with open("variables.pickle", "wb") as archivoPickle:
-		pickle.dump((palabras,tags, entrenamiento, salida))
-
-tensorflow.reset_default_graph()
-
-red = tflearn.input_data(shape=[None, len(entrenamiento[0])])
+entrenamiento = numpy.array(entrenamiento)
+salida = numpy.array(salida)
+ops.reset_default_graph()
+red = tflearn.input_data(shape=[None,len(entrenamiento[0])])
 red = tflearn.fully_connected(red,10)
 red = tflearn.fully_connected(red,10)
-
 red = tflearn.fully_connected(red,len(salida[0]),activation="softmax")
 red = tflearn.regression(red)
-
 modelo = tflearn.DNN(red)
-try:
-	modelo.load("modelo.tflearn")
-except:
-	modelo.fit(entrenamiento,salida,n_epoch=1000, batch_size= 10, show_metric=True)
-	modelo.save("modelo.tflearn")
+modelo.fit(entrenamiento,salida,n_epoch=1000,batch_size=10,show_metric=True)
+modelo.save("modelo.tflearn")
 
 def mainBot():
-	while  True:
-		entrada = input("Tu: ")
+	while True:
+		entrada = input("tu: ")
 		cubeta = [0 for _ in range(len(palabras))]
 		entradaProcesada = nltk.word_tokenize(entrada)
-		entradaProcesada = [stemmer.stem(palabra.lower()) for palabra in entradaProcesada]
+		entradaProcesada = [stemmer.stem(palabra.lower()) for palabra in
+		entradaProcesada]
 		for palabraIndividual in entradaProcesada:
-			for i, palabra in enumerate(palabras):
+			for i,palabra in enumerate(palabras):
 				if palabra == palabraIndividual:
-					cubeta[i] = 1 
+					cubeta[i] = 1
 		resultados = modelo.predict([numpy.array(cubeta)])
 		resultadosIndices = numpy.argmax(resultados)
-		tags = tags[resultadosIndices]
-
+		tag = tags[resultadosIndices]
 		for tagAux in datos["contenido"]:
-			if tagAux["tag"] ==tag:
+			if tagAux["tag"] == tag:
 				respuesta = tagAux["respuestas"]
+		print("BOT: ",random.choice(respuesta))
 mainBot()
